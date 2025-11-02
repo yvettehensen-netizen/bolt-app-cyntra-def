c4eea658b66745ec96b5800060a622397aa3860f
yvettehensen-netizen <yvettehensen@gmail.com>
2025-11-02 17:42:39 +0100

    Create App component for Cyntra Insights Portal

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      fontFamily: "sans-serif",
+      color: "#D6B48E",
+      backgroundColor: "#3B0E18"
+    }}>
+      <h1>Cyntra Insights Portal</h1>
+    </div>
+  );
+}
